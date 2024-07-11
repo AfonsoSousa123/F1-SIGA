aecfb8455efffddac6f6c9d6f0c74330312b8984
Afonso Sousa <afonsosousa10@gmail.com>
2024-07-11 07:57:58 +0100

    Changed the name

--- src/Components/Features/IntroVideo.tsx
+++ src/Components/Features/IntroVideo.tsx
@@ -3,11 +3,10 @@ const IntroVideo = () => {
     <>
       <div>
         <iframe
-          width="560"
+          width="auto"
           height="315"
           src="https://www.youtube.com/embed/yAYKH8NOSlQ?si=eE0P4qLqiu7150vz"
           title="YouTube video player"
-          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           referrerPolicy="strict-origin-when-cross-origin"
           allowFullScreen
