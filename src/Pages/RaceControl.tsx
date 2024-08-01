96f616c9d21d49a356a2b948e4046fa0ada5b1cd
AfonsoSousa123 <afonsosousa10@gmail.com>
2024-08-01 12:23:47 +0100

    Added Sort options to the RaceControl page

--- src/Pages/RaceControl.tsx
+++ src/Pages/RaceControl.tsx
@@ -1,6 +1,14 @@
 import { useEffect, useState } from "react";
 import { Loader } from "../Components/Navigation/Loader.tsx";
 import { fullTimeFormater } from "../misc/date-formater/date -formater.ts";
+import Box from "@mui/material/Box";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
 
 interface raceControlInfoProps {
   id: number;
@@ -34,44 +42,74 @@ export const RaceControl = () => {
   const [sessions, setSession] = useState<sessionProps[]>([]);
   const [error, setError] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [sortType, setSortType] = useState("descending");
 
   useEffect(() => {
-    async function fetchData() {
-      try {
-        const data = await fetchRaceControl();
-        const sessionData = await fetchSession();
-        setRaceControl(data);
-        setSession(sessionData);
-      } catch (error) {
-        // @ts-ignore
-        setError(error.message);
-      } finally {
-        setIsLoading(false);
-      }
+    fetchData();
+  }, [sortType]);
+
+  async function fetchData() {
+    try {
+      const data = await fetchRaceControl();
+      const sessionData = await fetchSession();
+      setSession(sessionData);
+      sortData(data);
+    } catch (error) {
+      // @ts-ignore
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  // using data from parameter instead of state
+  function sortData(data: raceControlInfoProps[]) {
+    let sortedData;
+    if (sortType === "descending") {
+      // Sorts the data by date in descending order
+      sortedData = [...data].sort((a, b) => {
+        const dateA = new Date(a.date);
+        const dateB = new Date(b.date);
+        return dateB.getTime() - dateA.getTime();
+      });
+    } else if (sortType === "ascending") {
+      // Sorts the data by date in ascending order
+      sortedData = [...data].sort((a, b) => {
+        const dateA = new Date(a.date);
+        const dateB = new Date(b.date);
+        return dateA.getTime() - dateB.getTime();
+      });
+    } else {
+      return data;
     }
 
-    fetchData();
-  }, []);
+    setRaceControl(sortedData);
+  }
 
   if (error) {
     return <div>Error: {error}</div>;
   }
 
+  // fetches the race control data
   async function fetchRaceControl(): Promise<raceControlInfoProps[]> {
     const response = await fetch(
       "https://api.openf1.org/v1/race_control?session_key=latest&meeting_key=latest",
     );
-    const data = await response.json();
-    return data;
+    return await response.json();
   }
 
+  // fetches the session data
   async function fetchSession(): Promise<sessionProps[]> {
     const response = await fetch(
       "https://api.openf1.org/v1/sessions?session_key=latest&meeting_key=latest",
     );
-    const data = await response.json();
-    return data;
+    return await response.json();
   }
+
+  // handles the change of the select component
+  const handleChange = (e: SelectChangeEvent) => {
+    setSortType(e.target.value as string);
+  };
 
   return (
     <>
@@ -92,6 +130,28 @@ export const RaceControl = () => {
               </div>
             );
           })}
+
+          <div className="wrapper">
+            <Box sx={{ minWidth: 80 }}>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Sort by</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  className={"select-sort"}
+                  id="demo-simple-select"
+                  value={sortType}
+                  label="Sort by"
+                  color={"primary"}
+                  onChange={handleChange}
+                >
+                  <MenuItem value={"descending"}>Descending</MenuItem>
+                  <MenuItem value={"ascending"}> Ascending</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+          </div>
+
+          <br />
 
           {raceControlInfo.map((raceControl) => {
             return (
