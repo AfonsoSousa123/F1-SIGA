188c795ada401bc0bc089f1073b352fe4a18de54
AfonsoSousa123 <afonsosousa10@gmail.com>
2024-08-01 11:45:57 +0100

    removed the AppBar

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import { TabsBar } from "./Components/Navigation/TabsBar.tsx";
 import Footer from "./Components/Navigation/Footer.tsx";
 import { Container } from "@mui/material";
 import Hero from "./Components/Features/Hero.tsx";
-import Menu from "./Components/Navigation/Menu.tsx";
+// import Menu from "./Components/Navigation/Menu.tsx";
 
 function App() {
   return (
     <>
       {/*Header Section*/}
-      <Menu></Menu>
+      {/*<Menu></Menu>*/}
       {/*Main Content Section*/}
       <Container>
         <Hero />
